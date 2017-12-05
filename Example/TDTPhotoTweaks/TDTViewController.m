4d85821f5acff9ce92abd595642d4e7bd704ccd0
dinesh.kumar <dinesh.ku@flock.com>
2017-12-05 19:58:18 +0530

    Prepare the sample barebones

--- Example/TDTPhotoTweaks/TDTViewController.m
+++ Example/TDTPhotoTweaks/TDTViewController.m
@@ -8,22 +8,55 @@
 
 #import "TDTViewController.h"
 
-@interface TDTViewController ()
+static NSString * const SampleImageName = @"sample_image";
+static NSString * const BarButtonTitleCrop = @"Crop";
+static NSString * const BarButtonTitleReset = @"Reset";
 
+@interface TDTViewController ()
+{
+  UIImageView * _imageView;
+}
 @end
 
 @implementation TDTViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+- (void)setupImageView {
+  _imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 250, 250)];
+  _imageView.center = self.view.center;
+  _imageView.layer.cornerRadius = 3.0;
+  _imageView.layer.masksToBounds = YES;
+  _imageView.contentMode = UIViewContentModeScaleAspectFill;
+  [self.view addSubview:_imageView];
 }
 
-- (void)didReceiveMemoryWarning
+- (void)resetImageInImageView {
+  [_imageView setImage:[UIImage imageNamed:SampleImageName]];
+}
+
+- (void)viewDidLoad
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+  [self.view setBackgroundColor:[UIColor blackColor]];
+  [super viewDidLoad];
+  [self setupNavigationItem];
+  [self setupImageView];
+  [self resetImageInImageView];
+}
+
+- (void)setupNavigationItem {
+  [self.navigationItem setTitle:@"Sample"];
+  UIBarButtonItem * cropButton = [[UIBarButtonItem alloc] initWithTitle:BarButtonTitleCrop style:UIBarButtonItemStylePlain target:self action:@selector(cropBarButtonTapped)];
+  UIBarButtonItem * resetButton = [[UIBarButtonItem alloc] initWithTitle:BarButtonTitleReset style:UIBarButtonItemStylePlain target:self action:@selector(resetBarButtonTapped)];
+  self.navigationItem.rightBarButtonItem = cropButton;
+  self.navigationItem.leftBarButtonItem = resetButton;
+}
+
+/// Bar button actions
+- (void)cropBarButtonTapped {
+  
+}
+
+- (void)resetBarButtonTapped {
+  [self resetImageInImageView];
 }
 
 @end
