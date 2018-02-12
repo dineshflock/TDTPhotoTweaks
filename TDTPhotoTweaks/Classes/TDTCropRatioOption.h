4d5aa2fbd7b3ec4fcc71ce0cc30579fa324b36ef
Dinesh Kumar <dinesh.ku@flock.com>
2018-02-12 17:31:32 +0530

    Create crop options class

---
+++ TDTPhotoTweaks/Classes/TDTCropRatioOption.h
@@ -0,0 +1,20 @@
+//
+//  TDTCropRatioOption.h
+//  Pods
+//
+//  Created by Dinesh Kumar on 2/12/18.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TDTCropRatioOption : NSObject
+
+@property (nonatomic, strong, readonly) NSString *name;
+@property (nonatomic, assign, readonly) CGFloat widthToHeightRatio;
+
+- (instancetype)initWithName:(NSString *)name
+            widthHeightRatio:(CGFloat)widthToHeightRatio;
+
++ (instancetype)cropOptionNone;
+
+@end
